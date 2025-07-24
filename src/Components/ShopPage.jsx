2e169c370735760ce7f276b4ab10151e25b6922c
lalit-machra <lalitmachra0403@gmail.com>
2025-07-24 06:39:30 +0530

    Fetch and display products in shop page using FakeStore API

--- src/Components/ShopPage.jsx
+++ src/Components/ShopPage.jsx
@@ -1,32 +1,38 @@
-import { default as Navbar } from './Navbar'
+import { default as Navbar } from './Navbar';
+import { useState, useEffect } from 'react';
 
 function ShopPage() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const data = await (await fetch('https://fakestoreapi.com/products')).json();
+      let dataArr = []
+      data.slice(0, 12).forEach((item) => {
+        dataArr.push(item);
+      });
+      setItems(dataArr);
+    }
+    getData();
+  }, []);
+  console.log(items)
+  let domData = items.map((item, index) => {
+    return (
+    <div className="item" key={index}>
+      <img src={item.image} className="itemImg"></img>
+      <h2>{item.title}</h2>
+      <h3>${item.price}</h3>
+      <label>Quantity: </label>
+      <input type="number" className="itemCount" defaultValue="1" min="1"></input>
+      <button className="addToCartBtn">Add to Cart</button>
+    </div>);
+  });
+
   return (
     <>
       <Navbar></Navbar>
       <h1>ABc Online Store</h1>
       <div className="items">
-        <div className="item">
-          <img src="src/images/product1.jpeg" className="itemImg"></img>
-          <h2>Item1</h2>
-          <label>Quantity: </label>
-          <input type="number" className="itemCount" defaultValue="1" min="1"></input>
-          <button className="addToCartBtn">Add to Cart</button>
-        </div>
-        <div className="item">
-          <img src="src/images/product1.jpeg" className="itemImg"></img>
-          <h2>Item2</h2>
-          <label>Quantity: </label>
-          <input type="number" className="itemCount" defaultValue="1" min="1"></input>
-          <button className="addToCartBtn">Add to Cart</button>
-        </div>
-        <div className="item">
-          <img src="src/images/product1.jpeg" className="itemImg"></img>
-          <h2>Item3</h2>
-          <label>Quantity: </label>
-          <input type="number" className="itemCount" defaultValue="1" min="1"></input>
-          <button className="addToCartBtn">Add to Cart</button>
-        </div>
+        {domData}
       </div>
     </>
   );
